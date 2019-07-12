7c9d9e6471a02858cb8a25218465306917abff53
E.J. Woerlee <egbert-jan.woerlee@cgm.com>
2019-07-12 16:16:16 +0200

    changes friday 12-07-2019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+@Component({
+   selector: 'app-sidebar',
+   template: `
+     <div id="sidebar">
+        Sidebar will go here
+     </div>
+  `
+})
+export class SidebarComponent {
+}
+
+@Component({
+  selector: 'app-article',
+  template: `
+  <div>
+    <h2> {{ article.title }} </h2>
+  </div>
+ `
+})
+export class ArticleComponent {
+  @Input() article: object;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div id="containter">
+      <app-sidebar></app-sidebar>
+      <div id="content">
+        <div>
+          <app-article
+          *ngFor="let article of articles"
+            [article]="article"></app-article>
+        </div>
+      </div>
+    </div>
+  `
 })
 export class AppComponent {
+  articles: object[];
   title = 'Reddit-clone';
+
+  constructor() {
+    this.articles =
+    [{
+        title: 'The angular 2 screencast',
+        description: 'The easiest way to learn ng'
+      }, {
+        title: 'Fullstack React',
+        description: 'This is react'
+      }, {
+        title: 'Vue',
+        description: 'Another javascript framework'
+      }, {
+        title: 'Test1',
+        description: 'Description test1'
+      }, {
+        title: 'Test2',
+        description: 'Description test2'
+    }];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, SidebarComponent, ArticleComponent  } from './app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
